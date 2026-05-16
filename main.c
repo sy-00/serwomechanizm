499a191a1981150d0dab3dec712eb7aa69cf26f1
sy-00 <sylwia3p@wp.pl>
2026-05-16 21:16:45 +0200

    feat: main

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "keyboard.h"
+#include "servo.h"
+
+
+int main ()
+{
+	KeyboardInit();
+	ServoInit(20);
+//	ServoGoTo(24);			--> Zadanie 2
+//	ServoGoTo(12);
+	
+	while(1)
+	{
+		
+		switch(eKeyboardRead())
+		{
+			case BUTTON_0:
+				ServoCallib();
+				break;
+			case BUTTON_1:
+				ServoGoTo(12);
+				break;
+			case BUTTON_2:
+				ServoGoTo(24);
+				break;
+			case BUTTON_3:
+				ServoGoTo(36);
+				break;
+			case RELASED:
+				break;
+		}
+	}
+}
