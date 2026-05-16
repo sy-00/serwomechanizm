0d46dcada17804b37d9fda5fce87f3c423dfb9ef
sy-00 <sylwia3p@wp.pl>
2026-05-16 17:14:42 +0200

    feat: basic modules to main branch

---
+++ timer.c
@@ -0,0 +1,52 @@
+#include <LPC21xx.H>
+
+#define TIMER_RESET 0x2
+#define TIMER_ENABLE 0x1
+#define MATCH_CONTROL_INTERRUPT 0x1
+#define MATCH_CONTROL_RESET 0x2
+#define MATCH_CONTROL_STOP 0x4
+#define INTERRUPT_RESET 0x1
+
+
+void InitTimer0()
+{
+	
+	T0TCR = TIMER_ENABLE;
+	
+}
+
+void WaitOnTimer0(unsigned int uiTime)
+{
+	
+	T0MR0 = uiTime * 15;
+	T0MCR = MATCH_CONTROL_STOP;
+	
+	T0TCR |= TIMER_RESET;
+	T0TCR = TIMER_ENABLE;
+	
+	while(T0TC != (uiTime *15))
+	{
+		
+	}
+
+}
+
+void InitTimer0Match0(unsigned int iDelayTime)
+{
+	
+	T0MR0 = iDelayTime * 15;
+	T0MCR = MATCH_CONTROL_INTERRUPT | MATCH_CONTROL_RESET;
+	T0TCR |= TIMER_RESET;
+	T0TCR = TIMER_ENABLE;
+	
+}
+
+void WaitOnTimer0Match0()
+{
+	
+	while(T0IR != 1)
+	{
+		
+	}
+	T0IR = INTERRUPT_RESET;
+}
